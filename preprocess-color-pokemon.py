021984fc2bbf8bc2a95346e2ac99849a303da878
mmitzy <dani.bendav@gmail.com>
2025-11-14 15:57:05 +0200

    LAB with normalized ranges

--- preprocess-color-pokemon.py
+++ preprocess-color-pokemon.py
@@ -39,5 +39,21 @@ def compute_average_and_median_colors(image_path):
         "v": round(median_color_hsv_vec[2] / 255, 2)
     }
 
-    return avg_color_rgb, median_color_rgb, avg_color_hsv, median_color_hsv
+    image_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    avg_color_lab_vec = np.mean(image_lab, axis=(0, 1))
+    avg_color_lab = {
+        "l": round(avg_color_lab_vec[0] / 2.55, 2),
+        "a": round((avg_color_lab_vec[1] - 128) / 128, 2),
+        "b": round((avg_color_lab_vec[2] - 128) / 128, 2)
+    }
+
+    median_color_lab_vec = np.median(image_lab, axis=(0, 1))
+    median_color_lab = {
+        "l": round(median_color_lab_vec[0] / 2.55, 2),
+        "a": round((median_color_lab_vec[1] - 128) / 128, 2),
+        "b": round((median_color_lab_vec[2] - 128) / 128, 2)
+    }
+
+    return avg_color_rgb, median_color_rgb, avg_color_hsv, median_color_hsv, avg_color_lab, median_color_lab
+
 
